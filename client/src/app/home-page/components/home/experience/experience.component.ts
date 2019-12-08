912c923eb69a52830ce905c1ce34c501123d8bf7
inpokon <inpokon@gmail.com>
2019-12-08 13:34:00 +0800

    #add experience

--- client/src/app/home-page/components/home/experience/experience.component.ts
+++ client/src/app/home-page/components/home/experience/experience.component.ts
@@ -6,7 +6,210 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
-
+  experience = [
+    {
+      date: 'Декабрь 2019',
+      type: 'work',
+      variant: 'standard',
+      title: '"Мегамир IT"',
+      desc: 'Разработка ERP для сети стоматологических клиник "New Smile".',
+      tabs: [
+        'ReactJs',
+        '1c-Битрикс'
+      ]
+    },
+    {
+      date: 'Декабрь 2019',
+      type: 'Course',
+      variant: 'standard',
+      title: 'Redux',
+      desc: 'Курс: "Профессиональная Разработка".',
+      tabs: [
+        'Redux'
+      ]
+    },
+    {
+      date: 'Июль 2019',
+      type: 'Course',
+      variant: 'standard',
+      title: 'FullStack-Мастер',
+      desc: 'Курс: "Разработка CRM-системы".',
+      tabs: [
+        'Node.js',
+        'Express',
+        'Angular 6',
+      ]
+    },
+    {
+      date: 'Июнь 2019',
+      type: 'Course',
+      variant: 'min',
+      title: '',
+      desc: 'Курс: "Angular 4 c Нуля до Профи".',
+      tabs: []
+    },
+    {
+      date: 'Май 2019',
+      type: 'work',
+      variant: 'standard',
+      title: 'Разработка многостраничного сайта',
+      desc: 'для стомтологической клиники NewSmile.',
+      tabs: [
+        'Pug',
+        'Jquery',
+        'SCSS',
+        'Webpack',
+      ]
+    },
+    {
+      date: 'Март 2019',
+      type: 'work',
+      variant: 'standard',
+      title: 'Разработка интернет-магазина',
+      desc: 'для салонов обуви Flyboots.',
+      tabs: [
+        'Pug',
+        'Jquery',
+        'Webpack',
+      ]
+    },
+    {
+      date: 'Январь 2019',
+      type: 'Course',
+      variant: 'standard',
+      title: 'Pug',
+      desc: 'Всегда рад изучить новую технологию для более качественной разработке.',
+      tabs: [
+        'Pug',
+        'Webpack'
+      ]
+    },
+    {
+      date: 'Ноябрь 2018',
+      type: 'work',
+      variant: 'standard',
+      title: 'Разработка ERP',
+      desc: 'Начали командную работу по разрабоке ERP.',
+      tabs: [
+        'ReactJs',
+        '1C-Битрикс',
+        'Git',
+      ]
+    },
+    {
+      date: 'Сентябрь 2018',
+      type: 'Course',
+      variant: 'standard',
+      title: 'React',
+      desc: 'Курс: "Профессиональная Разработка".',
+      tabs: []
+    },
+    {
+      date: 'Август 2018',
+      type: 'work',
+      variant: 'standard',
+      title: '"Серебряный ключ"',
+      desc: 'Landing-page для коттеджного поселка.',
+      tabs: [
+        'Gulp',
+        'Jquery',
+        'Html',
+        'Scss',
+      ]
+    },
+    {
+      date: 'Июль 2018',
+      type: 'Course',
+      variant: 'min',
+      title: '',
+      desc: 'Курс: "Программирование на PHP".',
+      tabs: []
+    },
+    {
+      date: 'Июнь 2018',
+      type: 'Course',
+      variant: 'min',
+      title: '',
+      desc: 'Курс: "1С-Битрикс ПРАКТИКА СОЗДАНИЯ ВЕБ-ПРОЕКТОВ".',
+      tabs: []
+    },
+    {
+      date: 'Май 2018',
+      type: 'Course',
+      variant: 'min',
+      title: '',
+      desc: 'Изучение с наставником JavaScript, БЭМ, Git.',
+      tabs: []
+    },
+    {
+      date: 'Апрель 2018',
+      type: 'work',
+      variant: 'standard',
+      title: 'Frontend разработчик "Мегамир IT"',
+      desc: 'Устроился Frontend разработчиком в студию по разрабоке и тех.поддержки сайтов "Мегамир IT".',
+      tabs: []
+    },
+    {
+      date: 'Март 2018',
+      type: 'Course',
+      variant: 'standard',
+      title: 'Jquery для верстки',
+      desc: '',
+      tabs: [
+        'Bower',
+        'npm',
+        'jquery',
+      ]
+    },
+    {
+      date: 'Февраль 2018',
+      type: 'Course',
+      variant: 'standard',
+      title: 'Wordpress для верстальщиков',
+      desc: '',
+      tabs: [
+        'Wordpress',
+        'php'
+      ]
+    },
+    {
+      date: 'Январь 2018',
+      type: 'Course',
+      variant: 'standard',
+      title: 'Академия верстки: Веб-разработчик',
+      desc: '',
+      tabs: [
+        'Html',
+        'CSS',
+        'Gulp',
+        'JS',
+      ]
+    },
+    {
+      date: 'Декабрь 2017',
+      type: 'Course',
+      variant: 'standard',
+      title: 'Академия верстки: Веб-разработчик',
+      desc: '',
+      tabs: [
+        'Html',
+        'CSS',
+        'Gulp',
+        'JS',
+      ]
+    },
+    {
+      date: 'Ноябрь 2017',
+      type: 'Course',
+      variant: 'standard',
+      title: 'HTML-Academy',
+      desc: 'Прохождение курсов и тестов.',
+      tabs: [
+        'HTML',
+        'CSS'
+      ]
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
--- client/src/app/home-page/components/home/main/main.component.ts
+++ client/src/app/home-page/components/home/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  slider = [
+    {
+      code: 'react',
+      sliderTitle: 'React Js',
+      circleTitle: 'ERP',
+      donutTitle: 'Web ERP',
+      desc: 'Решения с единой базой данных, которая находится на основном сервере, работает гараздо быстрее, при этом нагрузка на веб-сервер значительно ниже, а трафика потребляется меньше, чем при терминальном доступе.'
+    },
+    {
+      code: 'angular',
+      sliderTitle: 'Angular',
+      circleTitle: 'SPA',
+      donutTitle: 'Web-приложения',
+      desc: 'Размещенное на одной web-странице, которая для обеспечения работы загружает весь необходимый код вместе с загрузкой самой страницы.'
+    },
+    {
+      code: 'webpack',
+      sliderTitle: 'JavaScript',
+      circleTitle: 'JS',
+      donutTitle: 'Web-приложения',
+      desc: 'Все современные браузеры выбрали JavaScript'
+    }
+  ];
 
-  ngOnInit() {
+  sliderTitle: string;
+  circleTitle: string;
+  donutTitle: string;
+  desc: string;
+  svg: string;
+  count: number = 0;
+  activeClass: string = '';
+
+  constructor() {
+    this.editSlider();
+  }
+
+  ngOnInit(): void {
+    this.mySlider();
+  }
+
+  mySlider() {
+    setTimeout(() => {
+      this.count += 1;
+      if (this.count >= this.slider.length) {
+        this.count = 0;
+      }
+      this.activeClass = 'active';
+      setTimeout(() => {
+        this.editSlider();
+        this.activeClass = '';
+      }, 600);
+      this.mySlider();
+    }, 10000)
+  }
+
+  editSlider() {
+    this.sliderTitle = this.slider[this.count].sliderTitle;
+    this.circleTitle = this.slider[this.count].circleTitle;
+    this.donutTitle = this.slider[this.count].donutTitle;
+    this.desc = this.slider[this.count].desc;
+    this.svg = this.slider[this.count].code;
   }
 
 }
