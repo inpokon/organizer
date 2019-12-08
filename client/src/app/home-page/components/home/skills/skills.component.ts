cdf40215c6116c0df569e84e159031787f129584
inpokon <inpokon@gmail.com>
2019-12-08 18:36:08 +0800

    #fix skills

--- client/src/app/home-page/components/home/skills/skills.component.ts
+++ client/src/app/home-page/components/home/skills/skills.component.ts
@@ -7,7 +7,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  items;
+
+  sliders;
+
+  width: number;
+
+  constructor() {
+    this.items = [
+      {
+        name: 'webpack',
+        activeClass: false
+      },
+      {
+        name: 'js',
+        activeClass: false
+      },
+      {
+        name: 'react',
+        activeClass: true
+      },
+      {
+        name: 'angular',
+        activeClass: false
+      },
+      {
+        name: '1c_bitrix',
+        activeClass: false
+      },
+      {
+        name: 'gulp',
+        activeClass: false
+      },
+      {
+        name: 'boostrap',
+        activeClass: false
+      },
+      {
+        name: 'html5',
+        activeClass: false
+      },
+      {
+        name: 'jquery',
+        activeClass: false
+      },
+      {
+        name: 'sass',
+        activeClass: false
+      },
+      {
+        name: 'wordpress',
+        activeClass: false
+      },
+      {
+        name: 'git',
+        activeClass: false
+      },
+      {
+        name: 'css3',
+        activeClass: false
+      }
+    ];
+    this.width = 1000;
+    const windowWidth = window.screen.width;
+    if (windowWidth > 768 && windowWidth <= 1024) this.width = 650;
+    if (windowWidth >= 480 && windowWidth <= 768) this.width = 400;
+    if (windowWidth < 480) this.width = 320;
+
+  }
 
   ngOnInit() {
   }
